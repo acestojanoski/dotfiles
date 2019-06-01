4ef27a1263e75fb16bdc9475dcb87ccaabaab3e3
Aleksandar Stojanoski <aleksandar-stojanoski@hotmail.com>
2019-06-01 16:13:20 +0200

    i3

---
+++ github-push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git remote set-url origin https://github.com/acestojanoski/config-files.git
+git push origin master
+git remote set-url origin https://gitlab.com/acestojanoski/config-files.git
